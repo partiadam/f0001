8232f07ef53b5fcb66ccba55d50093098f2c9d2f
partiadam <71691758+partiadam@users.noreply.github.com>
2020-10-16 08:28:57 +0200

    Add files via upload

---
+++ f0001a.py
@@ -0,0 +1,6 @@
+#F0001a: Mi történik, ha lefuttatjuk ezt a programot? A program futtatása nélkül fogalmazd meg magadnak – aztán persze lefuttathatod, hogy lásd, igazad volt-e:)
+
+1
+2
+print('...Hogyha én ezt tudtam volna')
+print('Köpönyeget hoztam vóna!')
